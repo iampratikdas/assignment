4bf2f334ce1bb4c5be968aeacf580ef92fe7633f
iampratikdas <pratikdas967@gmail.com>
2024-04-26 16:27:14 +0530

    read me added

--- controllers/userController.js
+++ controllers/userController.js
@@ -17,17 +17,20 @@ connectDB.once('open', () => {
 
 exports.login = async (req, res) => {
   try {
-    const user = await User.findOne({ email: req.body.email });
-    if (!user) {
+    const user =  await User.aggregate([
+      { $match: { email: req.body.email } }
+    ]);
+    if (user.length === 0) {
       return res.status(404).send('User not found');
     }
-    const validPassword = await bcrypt.compare(req.body.password, user.password);
+    const validPassword = await bcrypt.compare(req.body.password, user[0].password);
     if (!validPassword) {
       return res.status(401).send('Invalid password');
     }
-    let token = await GenUserToken(user, moment().unix());
-    res.status(200).json({  user_details: user , token: token});
+    let token = await GenUserToken(user[0], moment().unix());
+    res.status(200).json({  user_details: user[0] , token: token});
   } catch (error) {
+    console.log("user===>", error)
     res.status(500).send('Error logging in');
   }
 };
